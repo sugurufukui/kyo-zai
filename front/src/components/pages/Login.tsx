30b561394fc2cafec332cdafaafa16870c81481e
sugurufukui <2ar.1225@gmail.com>
2022-12-04 22:00:54 +0900

    update: ログイン画面作成

--- front/src/components/pages/Login.tsx
+++ front/src/components/pages/Login.tsx
@@ -1,5 +1,29 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
 import { FC, memo } from "react";
 
 export const Login: FC = memo(() => {
-  return <p>ログインページです</p>;
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" p="4" borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          特別支援教育教材検索アプリ
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={6} py={4} px={10}>
+          <Input placeholder="ユーザーID" />
+          <Button colorScheme="teal" _hover={{ opacity: 0.8 }}>
+            ログイン
+          </Button>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
