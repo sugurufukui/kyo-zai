87cf125f68bcd17ba1330d498ea05492788a5bfc
sugurufukui <2ar.1225@gmail.com>
2022-12-15 06:54:28 +0900

    add: パスワード入力画面追加

--- front/src/components/organisms/layout/Header.tsx
+++ front/src/components/organisms/layout/Header.tsx
@@ -1,10 +1,94 @@
-import { Box, Flex, Heading, Link, useDisclosure } from "@chakra-ui/react";
-import { FC, memo } from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Link,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { FC, memo, useContext } from "react";
 import { MenuIconButton } from "../../atoms/button/MenuIconButton";
 import { MenuDrawer } from "../../molecules/MenuDrawer";
+import { signOut } from "../../../lib/api/auth/signOut";
+import Cookies from "js-cookie";
+import { useHistory } from "react-router-dom";
+
+// const useStyles = makeStyles((theme: Theme) => ({
+//   iconButton: {
+//     marginRight: theme.spacing(2),
+//   },
+//   title: {
+//     flexGrow: 1,
+//     textDecoration: "none",
+//     color: "inherit",
+//   },
+//   linkBtn: {
+//     textTransform: "none",
+//   },
+// }));
 
 export const Header: FC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  // const {} = useContext()
+
+  // const handleSignOut = () => {
+  //   try {
+  //     const res = await signOut()
+
+  //     if(res.data.success === true) {
+  //       //サインアウト時にはCookieを削除
+  //       Cookies.remove("_access_token")
+  //       Cookies.remove("_client")
+  //       Cookies.remove("_uid")
+
+  //       // setIssignedIn(false)
+
+  //     }
+  //   }
+  // }
+
+  const AuthButton = () => {
+    //認証完了時はサインアウト用のボタンを表示
+    //未認証時は認証用のボタンを表示
+    // if (!loading) {
+    //   if (isSignedIn) {
+    //     return (
+    //       <Button
+    //         color="inherit"
+    //         // className={classes.linkBtn}
+    //         onClick={handleSignOut}
+    //       >
+    //         ログアウト
+    //       </Button>
+    //     );
+    //   } else {
+    //     return (
+    //       <>
+    //         <Button
+    //           component={Link}
+    //           to="/signin"
+    //           color="inherit"
+    //           // className={classes.linkBtn}
+    //         >
+    //           ログイン
+    //         </Button>
+    //         <Button
+    //           component={Link}
+    //           to="/signup"
+    //           color="inherit"
+    //           // className={classes.linkBtn}
+    //         >
+    //           新規登録
+    //         </Button>
+    //       </>
+    //     );
+    //   }
+    // } else {
+    //   return <></>;
+    // }
+  };
+
   return (
     <>
       <Flex
