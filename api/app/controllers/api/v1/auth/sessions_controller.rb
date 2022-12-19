605bf792635ba4e0d1a93c5c0cb21f1d2993e95e
sugurufukui <2ar.1225@gmail.com>
2022-12-20 06:08:42 +0900

    add: レスポンスステータスを追加

--- api/app/controllers/api/v1/auth/sessions_controller.rb
+++ api/app/controllers/api/v1/auth/sessions_controller.rb
@@ -2,9 +2,9 @@
 class Api::V1::Auth::SessionsController < ApplicationController
   def index
     if current_api_v1_user
-      render json: { is_login: true, data: current_api_v1_user }
+      render json: { status: 200, current_user: current_api_v1_user }
     else
-      render json: { is_login: false, message: "ユーザーが存在しません" }
+      render json: { status: 500, message: "ユーザーが存在しません" }
     end
   end
 end
