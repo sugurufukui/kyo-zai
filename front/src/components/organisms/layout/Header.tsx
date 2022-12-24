7ba0da0414a055a591907ce63f6bd5c869023e55
sugurufukui <2ar.1225@gmail.com>
2022-12-24 19:19:20 +0900

    fix: chakraUIからmuiに変更

--- front/src/components/organisms/layout/Header.tsx
+++ front/src/components/organisms/layout/Header.tsx
@@ -1,123 +1,93 @@
-import {
-  Box,
-  Button,
-  Flex,
-  Heading,
-  Link,
-  useDisclosure,
-} from "@chakra-ui/react";
-import { FC, memo, useContext } from "react";
-import { MenuIconButton } from "../../atoms/button/MenuIconButton";
-import { MenuDrawer } from "../../molecules/MenuDrawer";
-import { signOut } from "../../../lib/api/auth/signOut";
+import React, { useContext } from "react";
+import { useHistory, Link } from "react-router-dom";
 import Cookies from "js-cookie";
-import { useHistory } from "react-router-dom";
 
-// const useStyles = makeStyles((theme: Theme) => ({
-//   iconButton: {
-//     marginRight: theme.spacing(2),
-//   },
-//   title: {
-//     flexGrow: 1,
-//     textDecoration: "none",
-//     color: "inherit",
-//   },
-//   linkBtn: {
-//     textTransform: "none",
-//   },
-// }));
+import AppBar from "@mui/material/AppBar";
+import Typography from "@mui/material/Typography";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+
+import { signOut } from "lib/api/auth/signOut";
+
+import { AuthContext } from "App";
+import { FC } from "react";
+import { memo } from "react";
+import { Box } from "@mui/material";
 
 export const Header: FC = memo(() => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { loading, isSignedIn, setIsSignedIn } = useContext(AuthContext);
+  // const { isOpen, onOpen, onClose } = useDisclosure();
+  // const { loading, isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
+  const histroy = useHistory();
 
-  // const {} = useContext()
+  const onClickSignOut = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    try {
+      const res = await signOut();
 
-  // const handleSignOut = () => {
-  //   try {
-  //     const res = await signOut()
+      if (res.data.success === true) {
+        // サインアウト時には各Cookieを削除
+        Cookies.remove("_access_token");
+        Cookies.remove("_client");
+        Cookies.remove("_uid");
 
-  //     if(res.data.success === true) {
-  //       //サインアウト時にはCookieを削除
-  //       Cookies.remove("_access_token")
-  //       Cookies.remove("_client")
-  //       Cookies.remove("_uid")
+        setIsSignedIn(false);
+        histroy.push("/signin");
+        //アラートのポップ画面表示（ログアウトしました）
+        alert("ログアウトしました");
+      } else {
+        alert("ログアウトできませんでした");
+        //アラートのポップ画面表示（ログアウトできませんでした）
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-  //       // setIssignedIn(false)
-
-  //     }
-  //   }
-  // }
-
-  const AuthButton = () => {
-    //認証完了時はサインアウト用のボタンを表示
-    //未認証時は認証用のボタンを表示
-    // if (!loading) {
-    //   if (isSignedIn) {
-    //     return (
-    //       <Button
-    //         color="inherit"
-    //         // className={classes.linkBtn}
-    //         onClick={handleSignOut}
-    //       >
-    //         ログアウト
-    //       </Button>
-    //     );
-    //   } else {
-    //     return (
-    //       <>
-    //         <Button
-    //           component={Link}
-    //           to="/signin"
-    //           color="inherit"
-    //           // className={classes.linkBtn}
-    //         >
-    //           ログイン
-    //         </Button>
-    //         <Button
-    //           component={Link}
-    //           to="/signup"
-    //           color="inherit"
-    //           // className={classes.linkBtn}
-    //         >
-    //           新規登録
-    //         </Button>
-    //       </>
-    //     );
-    //   }
-    // } else {
-    //   return <></>;
-    // }
+  const AuthButtons = () => {
+    // 認証完了後はサインアウト用のボタンを表示
+    // 未認証時は認証用のボタンを表示
+    if (!loading) {
+      //ボタンをprimaryButtonとしてcomponent化する
+      if (isSignedIn) {
+        return (
+          //ボタンをprimaryButtonとしてcomponent化する
+          //サインインしている状態のボタン=>①HOME②教材一覧③検索④投稿する⑤マイページ（アイコン（アイコンクリック時にモーダル出現））
+          <Button color="inherit" onClick={onClickSignOut}>
+            サインアウト
+          </Button>
+        );
+      } else {
+        return (
+          //ボタンをprimaryButtonとしてcomponent化する
+          //サインインしていない状態のボタン＝＞①HOME②教材一覧③検索④新規登録⑤ログイン
+          <Button component={Link} to="/signin" color="inherit">
+            ログイン
+          </Button>
+        );
+      }
+    } else {
+      return <></>;
+    }
   };
 
   return (
     <>
-      <Flex
-        as="nav"
-        bg="teal.500"
-        color="gray.50"
-        align="center"
-        justify="space-between"
-        padding={{ base: 3, md: 5 }}
-      >
-        <Flex align="center" as="a" _hover={{ cursor: "pointer" }}>
-          <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
-            きょーざい
-          </Heading>
-        </Flex>
+      {/* MenuDrawerでレスポンシブ対応させる */}
 
-        <Flex
-          align="center"
-          fontSize="sm"
-          display={{ base: "none", md: "flex" }}
-        >
-          <Box pr={4}>
-            <Link>教材一覧</Link>
-          </Box>
-          <Link>ユーザープロフィール</Link>
-        </Flex>
-        <MenuIconButton onOpen={onOpen} />
-      </Flex>
-      <MenuDrawer isOpen={isOpen} onClose={onClose} />
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar>
+          <Toolbar>
+            {/* スタイルを切り分ける */}
+            {/* 色変更する */}
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              {/* イラストを挿入する */}
+              {/* クリックするとホームへ戻るようにする */}
+              きょーざい
+            </Typography>
+            <AuthButtons />
+          </Toolbar>
+        </AppBar>
+      </Box>
     </>
   );
 });
