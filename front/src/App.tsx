da6d4da72403ef923882277de34fb94aa0b06471
sugurufukui <2ar.1225@gmail.com>
2022-12-24 19:30:16 +0900

    fix: isLoggedInをisSignedInに変更

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,54 +1,48 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import React, { useState, useEffect, createContext } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
-import theme from "./theme/theme";
-// import { Router } from "./router/Router";
-import React, {
-  createContext,
-  FC,
-  ReactElement,
-  useEffect,
-  useState,
-} from "react";
-import { User } from "./types/api/user";
-import { getCurrentUser } from "./lib/api/auth/getCurrentUser";
-import { HeaderLayout } from "./components/templates/HeaderLayout";
-import { homeRoutes } from "./router/HomeRoutes";
-import { Page404 } from "./components/pages/Page404";
-import { Login } from "./components/pages/Login";
+import { CommonLayout } from "components/templates/CommonLayout";
+import { Home } from "components/pages/Home";
+import { SignUp } from "components/pages/SignUp";
+import { SignIn } from "components/pages/SignIn";
 
-//グローバルで扱う変数・関数
+import { getCurrentUser } from "lib/api/auth/getCurrentUser";
+import { User } from "types/api/user";
+
+// グローバルで扱う変数・関数
 export const AuthContext = createContext(
   {} as {
     loading: boolean;
     setLoading: React.Dispatch<React.SetStateAction<boolean>>;
-    isLoggedIn: boolean;
-    setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+    isSignedIn: boolean;
+    setIsSignedIn: React.Dispatch<React.SetStateAction<boolean>>;
     currentUser: User | undefined;
     setCurrentUser: React.Dispatch<React.SetStateAction<User | undefined>>;
   }
 );
 
-export default function App() {
-  //セット関数を配置
+function App() {
   const [loading, setLoading] = useState<boolean>(true);
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
   const [currentUser, setCurrentUser] = useState<User | undefined>();
 
   // 認証済みのユーザーがいるかどうかチェック
-  // 確認できた場合はそのユーザー情報を取得
-  // axiosで書き換えたい
+  // 確認できた場合はそのユーザーの情報を取得
   const handleGetCurrentUser = async () => {
     try {
       const res = await getCurrentUser();
+      console.log(res);
 
-      if (res?.data.isLoggedIn === true) {
-        setIsLoggedIn(true);
-        setCurrentUser(res?.data.data);
-
-        console.log(res?.data.data);
+      if (res?.status === 200) {
+        setIsSignedIn(true);
+        setCurrentUser(res?.data.currentUser);
       } else {
-        console.log("ユーザーがいません");
+        alert("ユーザーがいません");
       }
     } catch (err) {
       console.log(err);
@@ -61,18 +55,14 @@ export default function App() {
     handleGetCurrentUser();
   }, [setCurrentUser]);
 
-  //ユーザーが認証済みがどうかでルーティングを決定
-  //未認証だった場合はログインページ（/login）に戻す
-  type Props = {
-    children: ReactElement;
-  };
-  const Private: FC<Props> = (props) => {
-    const { children } = props;
+  // ユーザーが認証済みかどうかでルーティングを決定
+  // 未認証だった場合は「/signin」ページに促す
+  const Private = ({ children }: { children: React.ReactElement }) => {
     if (!loading) {
-      if (isLoggedIn) {
+      if (isSignedIn) {
         return children;
       } else {
-        return <Redirect to="/" />;
+        return <Redirect to="/signin" />;
       }
     } else {
       return <></>;
@@ -80,50 +70,33 @@ export default function App() {
   };
 
   return (
-    <ChakraProvider theme={theme}>
-      <BrowserRouter>
-        {/* routerフォルダとしに切り分けたい */}
-        {/* AuthProviderとしてproviderフォルダに切り分けたい */}
-        <AuthContext.Provider
-          value={{
-            loading,
-            setLoading,
-            isLoggedIn,
-            setIsLoggedIn,
-            currentUser,
-            setCurrentUser,
-          }}
-        >
-          <HeaderLayout>
-            <Switch>
-              <Route exact path="/">
-                <Login />
-              </Route>
-              <Private>
-                <Route
-                  path="/home"
-                  render={({ match: { url } }) => (
-                    <Switch>
-                      {homeRoutes.map((route) => (
-                        <Route
-                          key={route.path}
-                          exact={route.exact}
-                          path={`${url}${route.path}`}
-                        >
-                          {route.children}
-                        </Route>
-                      ))}
-                    </Switch>
-                  )}
-                />
-              </Private>
-              <Route path="*">
-                <Page404 />
-              </Route>
-            </Switch>
-          </HeaderLayout>
-        </AuthContext.Provider>
-      </BrowserRouter>
-    </ChakraProvider>
+    <Router>
+      {/* routerフォルダとしに切り分けたい */}
+      {/* AuthProviderとしてproviderフォルダに切り分けたい */}
+      <AuthContext.Provider
+        value={{
+          loading,
+          setLoading,
+          isSignedIn,
+          setIsSignedIn,
+          currentUser,
+          setCurrentUser,
+        }}
+      >
+        <CommonLayout>
+          <Switch>
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/signin" component={SignIn} />
+            <Private>
+              <Switch>
+                <Route exact path="/" component={Home} />
+              </Switch>
+            </Private>
+          </Switch>
+        </CommonLayout>
+      </AuthContext.Provider>
+    </Router>
   );
 }
+
+export default App;
