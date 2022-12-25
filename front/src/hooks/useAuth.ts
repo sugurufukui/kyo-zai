38327769c5a3888611224b8a5f97c43d5c03a57a
sugurufukui <2ar.1225@gmail.com>
2022-12-26 01:03:54 +0900

    update: 認証済みユーザーの有無をカスタムフック化

---
+++ front/src/hooks/useAuth.ts
@@ -0,0 +1,74 @@
+// import axios from "axios";
+// import { useCallback, useState } from "react";
+// import { LoginParams } from "../types/api/LoginParams";
+// import { useHistory } from "react-router-dom";
+
+import { getCurrentUser } from "lib/api/auth/getCurrentUser";
+import { useEffect, useState } from "react";
+import { User } from "types/api/user";
+
+// export const useAuth = () => {
+//   const history = useHistory();
+
+//   //ローディング中に表示するもの
+//   const [loading, setLoading] = useState(false);
+
+//   const login = useCallback(() => {
+//     setLoading(true);
+//     axios
+//       .get<SignInparams>(`https://jsonplaceholder.typicode.com/users/${email}`)(`http://localhost:3000/api/v1/user/${email}`)
+//       .then((res) => {
+//         if (res.data) {
+//           history.push("/home");
+//         } else {
+//           alert("ユーザーが見つかりません");
+//         }
+//       })
+//       .catch((err) => alert("ログインできません"))
+//       .finally(() => setLoading(false));
+//   }, [history]);
+//   return { login };
+// };
+
+// 認証済みのユーザーがいるかどうかチェックするカスタムフック
+//useStateの内容を外部で使用できるようにフック化
+// 確認できた場合はそのユーザーの情報を取得
+export const useAuth = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
+  const [currentUser, setCurrentUser] = useState<User | undefined>();
+
+  //ここはaxiosで書きたい
+  const handleGetCurrentUser = async () => {
+    try {
+      const res = await getCurrentUser();
+      console.log(res);
+
+      if (res?.status === 200) {
+        setIsSignedIn(true);
+        setCurrentUser(res?.data.currentUser);
+      } else {
+        alert("ユーザーがいません");
+        console.log("ユーザーがいません");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+
+    setLoading(false);
+  };
+
+  //サインイン時にhandleGetCurrentUserしてユーザーの情報をもたせた方がいい？
+  useEffect(() => {
+    handleGetCurrentUser();
+  }, [setCurrentUser]);
+  return {
+    handleGetCurrentUser,
+    loading,
+    setLoading,
+    isSignedIn,
+    setIsSignedIn,
+    currentUser,
+    setCurrentUser,
+  };
+};
