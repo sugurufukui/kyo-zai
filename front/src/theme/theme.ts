58140734a91532dc28718fea52e93944886c288d
sugurufukui <2ar.1225@gmail.com>
2022-12-25 12:04:38 +0900

    update: defaultColorを変更

--- front/src/theme/theme.ts
+++ front/src/theme/theme.ts
@@ -1,13 +1,30 @@
-import { extendTheme } from "@chakra-ui/react";
+import { createTheme } from "@mui/material";
 
-const theme = extendTheme({
-  styles: {
-    global: {
-      body: {
-        backgroundColor: "gray.100",
-        color: "gray.800",
-      },
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#319795",
+      // light: "#33ab9f",
+      // dark: "#00695f",
     },
+    error: {
+      main: "#CE3256",
+    },
+    warning: {
+      main: "#F18D5F",
+    },
+    text: {
+      primary: "#1A202C",
+    },
+    background: {
+      // paper: "#fff",
+      default: "#EDF2F7",
+    },
+    // action: {
+    //   hover: "#00695f",
+    //   disabledBackground: "#CE3256",
+    // },
   },
 });
 export default theme;
