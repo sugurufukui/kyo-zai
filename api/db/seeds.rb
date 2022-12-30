88c26dcbb8700f8c1efd8e56c9d3a75b38e9e4ec
sugurufukui <2ar.1225@gmail.com>
2022-12-30 23:06:09 +0900

    add: Userの仮情報追加

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do |n|
+  User.create!(
+    # id: "id_#{n+1}",
+    uid: "test_#{n+1}@example.com",
+    provider: "provider_#{n+1}",
+    email: "test_#{n+1}@example.com",
+    name: "name_#{n+1}",
+    password: "password"
+    # nickname: "nickname_#{n+1}"
+    # image?: "id_#{n+1}",
+  )
+end
