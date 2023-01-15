3556c4a91546513099e2c2cf4b0f6849eaeefb51
sugurufukui <2ar.1225@gmail.com>
2023-01-16 06:56:39 +0900

    add: 教材のサンプルデータの追加

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,12 +1,19 @@
 5.times do |n|
   User.create!(
     # id: "id_#{n+1}",
-    uid: "test_#{n+1}@example.com",
-    provider: "provider_#{n+1}",
-    email: "test_#{n+1}@example.com",
-    name: "name_#{n+1}",
+    uid: "test_#{n}@example.com",
+    provider: "provider_#{n}",
+    email: "test_#{n}@example.com",
+    name: "name_#{n}",
     password: "password"
     # nickname: "nickname_#{n+1}"
     # image?: "id_#{n+1}",
+  )
+end
+
+5.times do |n|
+  Material.create!(
+    name: "教材_#{n}",
+    description:"これは教材_#{n}の説明文です。"
   )
 end
