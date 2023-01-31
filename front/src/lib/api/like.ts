576a9151ef28483bbeff2f8838e10e9880a7a76e
sugurufukui <2ar.1225@gmail.com>
2023-02-01 06:18:55 +0900

    add: Likeしているかどうか確認するエンドポイントを追加

--- front/src/lib/api/like.ts
+++ front/src/lib/api/like.ts
@@ -1,25 +1,33 @@
 import { client } from "lib/api/client";
 import Cookies from "js-cookie";
 
-// 新規作成
-export const createLike = (id) => {
-  return client.post(
-    `/materials/${id}/likes`,
-    {}, //createには、データを追加する箱が必要
-    {
-      // どのユーザーか判別するための情報
-      headers: {
-        "access-token": Cookies.get("_access_token"),
-        client: Cookies.get("_client"),
-        uid: Cookies.get("_uid"),
-      },
-    }
-  );
+// Likeする
+export const createLike = (id, params) => {
+  return client.post(`/materials/${id}/likes`, params, {
+    // どのユーザーか判別するための情報
+    headers: {
+      "access-token": Cookies.get("_access_token"),
+      client: Cookies.get("_client"),
+      uid: Cookies.get("_uid"),
+    },
+  });
 };
 
-// 削除
+// Likeを解除する
 export const deleteLike = (id) => {
-  return client.delete(`/likes/${id}`, {
+  return client.delete(`/materials/${id}/likes`, {
+    // どのユーザーか判別するための情報
+    headers: {
+      "access-token": Cookies.get("_access_token"),
+      client: Cookies.get("_client"),
+      uid: Cookies.get("_uid"),
+    },
+  });
+};
+
+//すでにLikeしているか確認する
+export const likedCheck = (id) => {
+  return client.get(`/materials/${id}/likes`, {
     // どのユーザーか判別するための情報
     headers: {
       "access-token": Cookies.get("_access_token"),
