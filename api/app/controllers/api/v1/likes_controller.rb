c1e23cab5de922a9fb543e5bb6cb11c8da7df788
sugurufukui <2ar.1225@gmail.com>
2023-02-02 07:31:43 +0900

    fix: showを削除

--- api/app/controllers/api/v1/likes_controller.rb
+++ api/app/controllers/api/v1/likes_controller.rb
@@ -1,50 +1,22 @@
 class Api::V1::LikesController < ApplicationController
-  before_action :authenticate_api_v1_user!, only: [:create, :destroy]
-
-  def show
-    if current_api_v1_user
-      @material = Material.find(params[:material_id])
-      @like = current_api_v1_user.likes.find_by(material_id: @material.id)
-      @like_count = Like.where(material_id: material.id)
-      @like_count = @like_count.length
-
-      render json: { like: @like, like_count: @like_count}
-    end
-  end
+  before_action :authenticate_api_v1_user!, only: ['create']
 
   def create
-    if current_api_v1_user
-      @material = Material.find(params[:material_id])
-      @like = Like.create(like_params)
+      like = Like.new(material_id: params[:id], user_id: current_api_v1_user.id)
 
       if like.save
-        render json: { status: :created, like: @like, }
+          render json: like
       else
-        render json: { status: 500, like: @like.errors, }
+          render json: like.errors, status: 422
       end
-    else
-      render json: {
-        status: 500,
-        errors: ["LIKEできませんでした"]
-      }
-    end
   end
 
   def destroy
-    @user = current_api_v1_user
-    @material = Material.find(params[:material_id])
-    @like = Like.find_by(material_id: @material.id, user_id: @user.id)
-
-    if @like.destroy
-      render json: { status: delete, like: @like, }
-    else
-      render json: { status: 500, like.errors, }
-    end
-  end
-
-  private
-
-  def like_params
-    params_permit(:material_id, :user_id)
+      like = Like.find_by(material_id: params[:id], user_id: current_api_v1_user.id)
+      if like.destroy
+          render json: like
+      else
+          render json: like.errors, status: 422
+      end
   end
 end
