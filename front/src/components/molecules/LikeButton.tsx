a774ff91812ffd4c5f4a1a8085d9bc16596133bb
sugurufukui <2ar.1225@gmail.com>
2023-02-02 07:42:44 +0900

    add: データ取得方法を変更

--- front/src/components/molecules/LikeButton.tsx
+++ front/src/components/molecules/LikeButton.tsx
@@ -1,184 +1,180 @@
-import { FC, memo, useContext, useEffect, useState } from "react";
+// import { FC, memo, useContext, useEffect, useState } from "react";
+// import IconButton from "@mui/material/IconButton";
+// import FavoriteIcon from "@mui/icons-material/Favorite";
+// import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+// import { createLike, deleteLike } from "lib/api/like";
+// import { useAllMaterials } from "hooks/useAllMaterials";
+// import { AuthContext } from "providers/AuthProvider";
+// import { Typography } from "@mui/material";
+// import { User } from "types/api/user";
+// import { MaterialType } from "types/api/materialType";
+
+// type Props = {
+//   //MaterialListに従って複数形のmaterialsに
+//   materialId: number | null;
+//   currentUser: User;
+//   initialLikeCount: number;
+// };
+
+// // いいねのボタンといいね数を表示するだけのもの
+// export const LikeButton: FC<Props> = memo((props) => {
+//   const { materialId, currentUser, initialLikeCount } = props;
+//   //materialのデータをカスタムフックから取得するか、context化してそこから持ってくるか
+//   // 他のコンポーネントではmaterialの情報をどのように受渡しているのか確認する
+//   // 一覧画面に表示させるためには一度materialCardを通すため、propsの受け渡しが煩雑になってしまう可能性がある、
+
+//   //いいねの🤍の色を管理
+//   const [liked, setLiked] = useState(false);
+
+//   //いいねの数を管理
+//   const [likeCount, setLikeCount] = useState(initialLikeCount);
+//   // 誰がどの教材にいいねしたのか
+//   const [likeData, setLikeData] = useState({
+//     userId: null,
+//     materialId: null,
+//   });
+
+//いいね追加時の挙動
+//いいねボタンを押したらいいねしているかを確認。
+// const handleGetLike = async () => {
+//   setLikeData({
+//     userId: String(currentUser.id),
+//     // Cannot read properties of undefined (reading 'id')
+//     materialId: materialId,
+//   });
+//   try {
+//     const res = await likedCheck(materialId);
+
+//     setLikeCount(res.data.likeCount);
+
+//     //likeデータがあればtrueにして色を変える
+//     if (res.data.like) {
+//       setLiked(true);
+//     }
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
+// const clickToLike = async () => {
+//   //現在のlikeの状態と逆の状態をchangeに代入
+//   //setLikesの更新。画面が更新される。changeを代入
+//   // change必要か？
+//   // そのままtrue,falseでよくないか？
+//   const change = true;
+//   setLiked(change);
+
+//   //いいねの数を＋1する
+//   try {
+//     const res = await createLike(materialId, likeData);
+//     setLikeCount(likeCount + 1);
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
+// //いいね解除時の挙動
+// const clickToUnLike = async () => {
+//   //現在のlikeの状態と逆の状態をchangeに代入
+//   //setLikesの更新。画面が更新される。changeを代入
+//   const change = false;
+//   setLiked(change);
+
+//   //いいねの数を-1にする
+//   try {
+//     const res = await deleteLike(materialId);
+//     console.log(res.data);
+//     setLikeCount(likeCount - 1);
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
+// useEffect(() => {
+//   handleGetLike();
+// }, [currentUser]);
+
+//   return (
+//     // likedがfalseであれば作成(true)にする（clickToLike）
+//     <>
+//       {/* {liked === false && (
+//         <IconButton onClick={() => clickToLike()} aria-label="いいねを追加する">
+//           <FavoriteBorderIcon />
+//         </IconButton>
+//       )} */}
+//       {liked === true && (
+//         <IconButton
+//           onClick={() => clickToUnLike()}
+//           aria-label="いいねを解除する"
+//         >
+//           <FavoriteIcon />
+//         </IconButton>
+//       )}
+//       <Typography>いいね{likeCount}</Typography>
+//     </>
+//   );
+// });
+
+import { FC, memo } from "react";
 import IconButton from "@mui/material/IconButton";
-import FavoriteIcon from "@mui/icons-material/Favorite";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import { createLike, deleteLike, likedCheck } from "lib/api/like";
-import { useAllMaterials } from "hooks/useAllMaterials";
-import { AuthContext } from "providers/AuthProvider";
-import { Typography } from "@mui/material";
+import { createLike, deleteLike } from "lib/api/like";
 import { User } from "types/api/user";
-import { MaterialType } from "types/api/materialType";
+import { getAllMaterial, getDetailMaterial } from "lib/api/material";
 
 type Props = {
-  //MaterialListに従って複数形のmaterialsに
-  materials: any;
+  // getDetailMaterial: any;
+  material: any;
   currentUser: User;
-  initialLikeCount: number;
 };
 
-// いいねのボタンといいね数を表示するだけのもの
 export const LikeButton: FC<Props> = memo((props) => {
-  const { materials, currentUser, initialLikeCount } = props;
-  //materialのデータをカスタムフックから取得するか、context化してそこから持ってくるか
-  // 他のコンポーネントではmaterialの情報をどのように受渡しているのか確認する
-  //componentではデータの受け渡しのみを行うため、currentUserなどはcurrent_userとしてそのまま渡す？
-  // 一覧画面に表示させるためには一度materialCardを通すため、propsの受け渡しが煩雑になってしまう可能性がある、
-  // const { materials } = useAllMaterials();
-  // const { currentUser } = useContext(AuthContext);
-
-  //いいねの🤍の色を管理
-  const [liked, setLiked] = useState(false);
-
-  //いいねの数を管理
-  const [likeCount, setLikeCount] = useState(initialLikeCount);
-  // 誰がどの教材にいいねしたのか
-  const [likeData, setLikeData] = useState({
-    userId: "",
-    materialId: "",
-  });
+  const { material, currentUser } = props;
 
   //いいね追加時の挙動
-  // 下記イイねボタン押下時の2つの挙動を一つにまとめることがは可能か
-  //いいねボタンを押したらいいねしているかを確認。
-  const handleGetLike = async () => {
-    setLikeData({
-      // 型をstring型に
-      userId: String(currentUser.id),
-      materialId: String(materials.id),
-    });
+  const handleCreateLike = async (item) => {
     try {
-      const res = await likedCheck(materials.id);
-
-      setLikeCount(res.data.likeCount);
-
-      //likeデータがあればtrueにして色を変える
-      if (res.data.like) {
-        setLiked(true);
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const clickToLike = async () => {
-    //現在のlikeの状態と逆の状態をchangeに代入
-    //setLikesの更新。画面が更新される。changeを代入
-    // change必要か？
-    // そのままtrue,falseでよくないか？
-    const change = true;
-    setLiked(change);
-
-    //いいねの数を＋1する
-    try {
-      const res = await createLike(materials.id, likeData);
-      setLikeCount(likeCount + 1);
+      const res = await createLike(item.id);
+      console.log(res.data);
+      // getDetailMaterial(); //教材データを取得
+      getAllMaterial(); //教材一覧を取得
     } catch (e) {
       console.log(e);
     }
   };
 
   //いいね解除時の挙動
-  const clickToUnLike = async () => {
-    //現在のlikeの状態と逆の状態をchangeに代入
-    //setLikesの更新。画面が更新される。changeを代入
-    const change = false;
-    setLiked(change);
-
-    //いいねの数を-1にする
+  const handleDeleteLike = async (material) => {
     try {
-      const res = await deleteLike(materials.id);
+      const res = await deleteLike(material.id);
       console.log(res.data);
-      setLikeCount(likeCount - 1);
+      // getDetailMaterial(); //教材データを取得]
+      getAllMaterial(); //教材一覧を取得
     } catch (e) {
       console.log(e);
     }
   };
-
-  useEffect(() => {
-    handleGetLike();
-  }, [currentUser]);
-
   return (
-    // likedがfalseであれば作成(true)にする（clickToLike）
     <>
-      {liked === false && (
-        <IconButton
-          onClick={() => clickToUnLike()}
-          aria-label="いいねを追加する"
-        >
-          <FavoriteBorderIcon />
-        </IconButton>
-      )}
-      {liked === true && (
-        <IconButton
-          onClick={() => clickToUnLike()}
-          aria-label="いいねを解除する"
-        >
-          <FavoriteIcon />
-        </IconButton>
-      )}
-      <Typography>いいね{likeCount}</Typography>
+      {material.likes.find((like) => like.userId === currentUser.id)} ? (
+      {/* likesの配列の中にcurrentUser.idがあればlikeを解除、なければ作成 */}
+      <IconButton
+        onClick={() => handleDeleteLike(material)}
+        aria-label="add to favorites"
+      >
+        <FavoriteBorderIcon />
+        {material.likes.length}
+      </IconButton>
+      ) :(
+      <IconButton
+        onClick={() => handleCreateLike(material)}
+        aria-label="add to favorites"
+      >
+        <FavoriteBorderIcon />
+        {material.likes.length}
+      </IconButton>
+      )
     </>
   );
 });
-
-// import { FC, memo } from "react";
-// import IconButton from "@mui/material/IconButton";
-// import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-// import { createLike, deleteLike } from "lib/api/like";
-// import { User } from "types/api/user";
-
-// type Props = {
-//   getDetailMaterial: any;
-//   material: any;
-//   currentUser: User;
-// };
-
-// export const LikeButton: FC<Props> = memo((props) => {
-//   const { getDetailMaterial, material, currentUser } = props;
-
-//   //いいね追加時の挙動
-//   const handleCreateLike = async (material) => {
-//     try {
-//       const res = await createLike(material.id);
-//       console.log(res.data);
-//       getDetailMaterial(); //教材データを取得
-//     } catch (e) {
-//       console.log(e);
-//     }
-//   };
-
-//   //いいね解除時の挙動
-//   const handleDeleteLike = async (material) => {
-//     try {
-//       const res = await deleteLike(material.id);
-//       console.log(res.data);
-//       getDetailMaterial(); //教材データを取得
-//     } catch (e) {
-//       console.log(e);
-//     }
-//   };
-//   return (
-//     <>
-//       {material.likes.find((like) => like.userId === currentUser.id)} ? (
-//       {/* likesの配列の中にcurrentUser.idがあればlikeを解除、なければ作成 */}
-//       <IconButton
-//         onClick={() => handleDeleteLike(material)}
-//         aria-label="add to favorites"
-//       >
-//         <FavoriteBorderIcon />
-//         {material.likes.length}
-//       </IconButton>
-//       ) :(
-//       <IconButton
-//         onClick={() => handleCreateLike(material)}
-//         aria-label="add to favorites"
-//       >
-//         <FavoriteBorderIcon />
-//         {material.likes.length}
-//       </IconButton>
-//       )
-//     </>
-//   );
-// });
-// //
+//
