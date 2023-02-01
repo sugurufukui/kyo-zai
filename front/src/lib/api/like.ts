cd296b0eb9a3bb332e36eb47fe49bc3ba444bcdd
sugurufukui <2ar.1225@gmail.com>
2023-02-02 07:40:48 +0900

    add: createにはデータ追加の箱を追加

--- front/src/lib/api/like.ts
+++ front/src/lib/api/like.ts
@@ -1,34 +1,24 @@
 import { client } from "lib/api/client";
 import Cookies from "js-cookie";
 
-// Likeする
-export const createLike = (id, params) => {
-  return client.post(`/materials/${id}/likes`, params, {
-    // どのユーザーか判別するための情報
-    headers: {
-      "access-token": Cookies.get("_access_token"),
-      client: Cookies.get("_client"),
-      uid: Cookies.get("_uid"),
-    },
-  });
+//LIKEする
+export const createLike = (id) => {
+  return client.post(
+    `/materials/${id}/likes`,
+    {}, // createにはデータ追加の箱が必要
+    {
+      headers: {
+        "access-token": Cookies.get("_access_token"),
+        client: Cookies.get("_client"),
+        uid: Cookies.get("_uid"),
+      },
+    }
+  );
 };
 
-// Likeを解除する
+// LIKEを解除する
 export const deleteLike = (id) => {
-  return client.delete(`/materials/${id}/likes`, {
-    // どのユーザーか判別するための情報
-    headers: {
-      "access-token": Cookies.get("_access_token"),
-      client: Cookies.get("_client"),
-      uid: Cookies.get("_uid"),
-    },
-  });
-};
-
-//すでにLikeしているか確認する
-export const likedCheck = (id) => {
-  return client.get(`/materials/${id}/likes`, {
-    // どのユーザーか判別するための情報
+  return client.delete(`/likes/${id}`, {
     headers: {
       "access-token": Cookies.get("_access_token"),
       client: Cookies.get("_client"),
