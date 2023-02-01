6e26393f6722eb9015a6e9320c7d6aaf287c2a2b
sugurufukui <2ar.1225@gmail.com>
2023-02-02 07:33:00 +0900

    fix: バリデーションを削除

--- api/app/models/like.rb
+++ api/app/models/like.rb
@@ -2,5 +2,5 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :material
 
-  validate_uniqueness_of :material_id, scope: :user_id
+  # validates_uniqueness_of :material_id, scope: :user_id
 end
