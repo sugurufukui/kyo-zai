3b6fbb7909059f79b16e7a127e3c246f4010b086
sugurufukui <2ar.1225@gmail.com>
2023-02-04 17:23:46 +0900

    add: LikeButtonといいねの有無を確認するAPIを追加

--- front/src/components/pages/MaterialDetail.tsx
+++ front/src/components/pages/MaterialDetail.tsx
@@ -6,46 +6,40 @@ import { deleteMaterial, getDetailMaterial } from "lib/api/material";
 import { useSnackbar } from "providers/SnackbarProvider";
 
 import { AuthContext } from "providers/AuthProvider";
-import { createLike, deleteLike } from "lib/api/like";
-import { Like } from "types/api/like";
+import { LikeButton } from "components/molecules/LikeButton";
+
+import { likedCheck } from "lib/api/like";
 
 export const MaterialDetail: FC = memo(() => {
   const history = useHistory();
 
   // { id = 1 } を取得する
-  const query = useParams();
+  const query: any = useParams();
 
   const [value, setValue] = useState({
     id: 0,
     name: "",
     description: "",
-    user: {
-      id: 0,
-      name: "",
-    },
-    likes: [],
+    userId: 0,
   });
 
-  // いいねの情報
-  const [likes, setLikes] = useState<Like[]>();
+  // いいねの数の情報
+  const [likeCount, setLikeCount] = useState(0);
+
   const { currentUser } = useContext(AuthContext);
   const { showSnackbar } = useSnackbar();
 
   //教材詳細API(materials_controllerのshowの中のjsonの内容を引用)
-
   const getDetail = async (query: any) => {
     try {
       const res = await getDetailMaterial(query.id);
       console.log(res.data);
-      // setValue(res.data);
       setValue({
         id: res.data.id,
         name: res.data.name,
         description: res.data.description,
-        user: { id: res.data.user.id, name: res.data.user.name },
-        likes: [],
+        userId: res.data.userId,
       });
-      setLikes(res.data.likes);
     } catch (e) {
       console.log(e);
     }
@@ -70,30 +64,12 @@ export const MaterialDetail: FC = memo(() => {
     }
   };
 
-  //いいね作成API
-  const handleCreateLike = async (item: MaterialType) => {
+  //いいね確認API
+  const handleGetLike = async () => {
     try {
-      const res = await createLike(item.id);
+      const res = await likedCheck(query.id);
       console.log(res.data);
-      // queryにするべき？
-      // Parameters: {"id"=>"[object Object]"}となっていたものはquery.idとすることで解決
-      getDetailMaterial(item.id);
-      // getDetailMaterial(query.id);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  // いいね削除API
-  const handleDeleteLike = async (item: MaterialType) => {
-    try {
-      const res = await deleteLike(item.id);
-      console.log(res.data);
-
-      // Parameters: {"id"=>"[object Object]"}となっていたものはquery.idとすることで解決
-      // queryにするべき？
-      getDetailMaterial(item.id);
-      // getDetailMaterial(query.id);
+      setLikeCount(res.data.likeCount);
     } catch (e) {
       console.log(e);
     }
@@ -104,6 +80,7 @@ export const MaterialDetail: FC = memo(() => {
   // useEffectの副作用を使って処理をもう一度実行させる
   useEffect(() => {
     getDetail(query);
+    handleGetLike();
   }, [query]);
 
   return (
@@ -113,32 +90,30 @@ export const MaterialDetail: FC = memo(() => {
       <div>教材のID:{value?.id}</div>
       <div>名前:{value?.name}</div>
       <div>説明:{value?.description}</div>
-      <div>作成者ID:{value?.user.id}</div>
-      <div>
-        {likes?.find((like) => like.userId === currentUser.id) ? (
-          <p onClick={() => handleDeleteLike(value)}>♡{likes?.length}</p>
-        ) : (
-          <p onClick={() => handleCreateLike(value)}>♡{likes?.length}</p>
-        )}
-      </div>
+      <div>作成者ID:{value?.userId}</div>
+
+      <LikeButton
+        materialId={query.id}
+        currentUser={currentUser}
+        initialLikeCount={likeCount}
+      />
 
       <button onClick={() => history.goBack()}>戻る</button>
       <button onClick={() => console.log(value)}>valueの値</button>
-      <button onClick={() => console.log(likes)}>likesの値</button>
       <button onClick={() => console.log(query)}>queryの値</button>
 
       <button onClick={() => history.push("/materials/new")}>新規登録</button>
 
       {/* ログインユーザーのIDと教材のユーザーIDが一致している場合に編集を表示  */}
-      {currentUser.id === value?.user.id ? (
+      {currentUser.id === value?.userId ? (
         <Link to={`/materials/edit/${value?.id}`}>編集</Link>
       ) : (
         <></>
       )}
       {/* ログインユーザーのIDと教材のユーザーIDが一致している場合に削除ボタンを表示  */}
 
-      {currentUser.id === value?.user.id ? (
-        <button onClick={() => onClickDelete(value)}>削除</button>
+      {currentUser.id === value.userId ? (
+        <button onClick={() => onClickDelete(query)}>削除</button>
       ) : (
         <></>
       )}
