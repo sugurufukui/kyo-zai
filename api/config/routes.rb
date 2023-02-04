b6f9cb0195e73974aba3a6c22f6bd0c515c956aa
sugurufukui <2ar.1225@gmail.com>
2023-02-04 16:53:27 +0900

    fix: likesのルーティングを変更

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,13 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users
       resources :materials do
-        member do
-        # /api/v1/materials/:id/likes
-        # resources :likes, only: %i[show create destroy]
-        resources :likes, only: %i[create]
-        end
+        resource :likes, only: %i[show create destroy]
       end
-      resources :likes, only: %i[destroy]
 
       # #自分がいいねした教材
       # get "/my_liked_materials", to: "materials#my_liked_materials", as: :my_liked_materials
