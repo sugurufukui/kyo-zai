07f15afd6fab4ef9f89af6eebb9b394f10b91a5c
sugurufukui <2ar.1225@gmail.com>
2023-02-16 06:04:24 +0900

    add: ページネーションを追加

--- front/src/components/pages/MaterialList.tsx
+++ front/src/components/pages/MaterialList.tsx
@@ -3,13 +3,15 @@ import { FC, memo, useCallback, useContext, useEffect, useState } from "react";
 // import { deleteMaterial } from "lib/api/material";
 // import { useSnackbar } from "providers/SnackbarProvider";
 import { MaterialCard } from "components/organisms/material/MaterialCard";
-import { Grid } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import { useAllMaterials } from "hooks/useAllMaterials";
 import { MaterialModal } from "components/organisms/material/MaterialModal";
 import { useSelectMaterial } from "hooks/useSelectMaterial";
 import { AuthContext } from "providers/AuthProvider";
 import { likedCheck } from "lib/api/like";
 import { useLike } from "hooks/useLike";
+import { Pagination } from "components/molecules/Pagination";
+import ReactPaginate from "react-paginate";
 // import { LikeButton } from "components/molecules/LikeButton";
 
 //ログインユーザー教材(/user/materials)一覧画面と共有している項目はコンポーネント化して切り分けたい！！
@@ -44,23 +46,35 @@ export const MaterialList: FC<Props> = memo((props) => {
   );
   const handleClose = () => setOpen(false);
 
-  // //いいね確認API
-  // const handleGetLike = async () => {
-  //   try {
-  //     // ここのlikedCheckのidが取れていないから、404エラーとなっている
-  //     const res = await likedCheck(materials);
-  //     console.log(res.data);
-  //     setLikeCount(res.data.likeCount);
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // };
-
   // 教材データの取得
   useEffect(() => {
     getMaterials();
     handleGetLike();
   }, [getMaterials, handleGetLike]);
+
+  // pagination関係
+
+  // 1ページに表示する数を指定
+  const itemsPerPage = 6;
+
+  // ページの先頭の教材(何番目のアイテムから表示するか)
+  const [itemsOffset, setItemsOffset] = useState(0);
+
+  // 次のページの先頭の教材 ページ番号＋1ページに表示する教材の数(6)
+  const endOffset = itemsOffset + itemsPerPage;
+  // 一つのページに表示する教材
+  const currentMaterials = materials?.slice(itemsOffset, endOffset);
+
+  // 全ページ数 ＝ 全教材数から1ページに表示する教材(6)を割った値を繰り上げた値
+  const pageCount = Math.ceil(materials?.length / itemsPerPage);
+
+  const handlePageClick = (e: { selected: number }) => {
+    // クリックされた値から -1したもの
+    console.log(e.selected);
+
+    const newOffset = (e.selected * itemsPerPage) % materials.length; // クリックした部分のページ数が{selected: 2}のような形で返ってくる
+    setItemsOffset(newOffset); // offsetを変更し、表示開始するアイテムの番号を変更
+  };
 
   return (
     <>
@@ -75,7 +89,7 @@ export const MaterialList: FC<Props> = memo((props) => {
           p: { xs: 3, md: 6 },
         }}
       >
-        {materials.map((material) => (
+        {currentMaterials.map((material) => (
           <Grid
             key={material.id}
             sx={{
@@ -105,6 +119,34 @@ export const MaterialList: FC<Props> = memo((props) => {
         // currentUser={currentUser}
         initialLikeCount={likeCount}
       />
+      {/* <Pagination /> */}
+      <Box sx={{ justifyContent: "center", textAlign: "center" }}>
+        <ReactPaginate
+          pageCount={pageCount}
+          onPageChange={handlePageClick} // クリック時のfunction
+          previousLabel={"<"}
+          nextLabel={">"}
+          breakLabel={"..."}
+          marginPagesDisplayed={5} // 一番最初と最後を基準にして、そこからいくつページ数を表示するか
+          pageRangeDisplayed={5} // アクティブなページを基準にして、そこからいくつページ数を表示するか
+          // containerClassName={"pagination"} // ページネーションであるulに着くクラス名
+          // subContainerClassName={"pages pagination"}
+          // activeClassName={"active"} // アクティブなページのliに着くクラス名
+          // previousClassName={"pagination__previous"} // 「<」のliに着けるクラス名
+          // nextClassName={"pagination__next"} // 「>」のliに着けるクラス名
+          disabledClassName={"pagination__disabled"} // 使用不可の「<,>」に着くクラス名
+          pageClassName="page-item"
+          pageLinkClassName="page-link"
+          previousClassName="page-item"
+          previousLinkClassName="page-link"
+          nextClassName="page-item"
+          nextLinkClassName="page-link"
+          breakClassName="page-item"
+          breakLinkClassName="page-link"
+          containerClassName="pagination"
+          activeClassName="active"
+        />
+      </Box>
     </>
   );
 });
