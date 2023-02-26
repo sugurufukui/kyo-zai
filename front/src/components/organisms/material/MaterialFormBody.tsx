b9619d869d7d6578aa08a9952279339579cc40d5
sugurufukui <2ar.1225@gmail.com>
2023-02-26 17:44:04 +0900

    add: 編集時に変更前の画像を表示

--- front/src/components/organisms/material/MaterialFormBody.tsx
+++ front/src/components/organisms/material/MaterialFormBody.tsx
@@ -3,6 +3,7 @@ import React, { FC, ReactNode, useCallback } from "react";
 import {
   Button,
   IconButton,
+  Paper,
   Skeleton,
   TextField,
   Typography,
@@ -10,6 +11,7 @@ import {
 import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
 import { Box } from "@mui/system";
 import CloseIcon from "@mui/icons-material/Close";
+import KeyboardDoubleArrowDownRoundedIcon from "@mui/icons-material/KeyboardDoubleArrowDownRounded";
 
 import { useSnackbar } from "providers/SnackbarProvider";
 
@@ -17,15 +19,15 @@ type Props = {
   value: any;
   onClickSubmit: any;
   children: ReactNode;
-  // resetFile: any;
   onChangeName: any;
   onChangeDescription: any;
-  // onChangeFileInput: any;
-  image: any;
+  image?: any;
   setImage: any;
-  preview: any;
+  preview?: any;
   setPreview: any;
   disabled: any;
+  onChangeImage?: any;
+  startIcon: any;
 };
 
 // 新規登録と編集のフォームと、画像プレビューを表示するコンポーネント
@@ -34,48 +36,56 @@ export const MaterialFormBody: FC<Props> = (props) => {
     onClickSubmit,
     children,
     value,
-    // resetFile,
     onChangeName,
     onChangeDescription,
-    // onChangeFileInput,
     image,
     setImage,
     preview,
     setPreview,
     disabled,
+    onChangeImage,
+    startIcon,
   } = props;
 
   const { showSnackbar } = useSnackbar();
   const isLoading = !image && !preview;
 
   // 画像選択機能
-  const uploadImage = useCallback((e) => {
-    const file = e.target.files[0];
-    // image以外のファイルはnullにしてプレビューさせずにアラート表示;
-    if (file.type.includes("image/")) {
-      setImage(file);
-      console.log(file);
-      console.log(image);
-    } else {
-      setImage(null);
-      showSnackbar("そのファイルは登録できません", "error");
-      return;
-    }
-  }, []);
+  const uploadImage = useCallback(
+    (e) => {
+      const file = e.target.files[0];
+      // image以外のファイルはnullにしてプレビューさせずにアラート表示;
+      if (file.type.includes("image/")) {
+        setImage(file);
+        console.log("file", file);
+        console.log("image", image);
+      } else {
+        setImage(null);
+        showSnackbar("そのファイルは登録できません", "error");
+        return;
+      }
+    },
+    [setImage, showSnackbar, image]
+  );
 
   // プレビュー機能
-  const previewImage = useCallback((e) => {
-    const file = e.target.files[0];
-    setPreview(window.URL.createObjectURL(file));
-    console.log(file);
-  }, []);
+  const previewImage = useCallback(
+    (e) => {
+      const file = e.target.files[0];
+      setPreview(window.URL.createObjectURL(file));
+      console.log("file", file);
+      console.log("value.file", value.file);
+    },
+    [setPreview]
+  );
 
   // 画像選択取り消し
   const resetFile = useCallback(() => {
     setImage(null);
     setPreview(null);
-    console.log(image, preview);
-  }, []);
+    console.log("image", image);
+    console.log("preview", preview);
+  }, [setImage, setPreview, image, preview]);
 
   return (
     <>
@@ -93,7 +103,6 @@ export const MaterialFormBody: FC<Props> = (props) => {
           onChange={onChangeName}
         />
         <TextField
-          autoFocus
           type="description"
           name="description"
           id="description"
@@ -103,30 +112,78 @@ export const MaterialFormBody: FC<Props> = (props) => {
           fullWidth
           rows="4"
           value={value.description}
-          // onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          //   setDescription(e.target.value);
-          // }}
           onChange={onChangeDescription}
         />
+
+        {/* 編集画面に表示 */}
         <div>
-          <label htmlFor="icon-button-file">
-            <input
-              accept="image/*"
-              id="icon-button-file"
-              type="file"
-              hidden
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                uploadImage(e);
-                previewImage(e);
-              }}
-              // onChange={onChangeFileInput}
-            />
-            <IconButton color="inherit" component="span">
-              <PhotoCameraIcon />
-              <Typography>教材の写真を選択</Typography>
-            </IconButton>
-          </label>
+          {/* 変更前の写真を表示 */}
+          {value.image ? (
+            <>
+              <Typography>変更前</Typography>
+              <div>
+                <Box>
+                  <img
+                    src={value.image?.url}
+                    alt="変更前の写真"
+                    width={(260 * 4) / 3}
+                    height={260}
+                  />
+                </Box>
+              </div>
+              <Box sx={{ m: 4, height: 0, textAlign: "center" }}>
+                <KeyboardDoubleArrowDownRoundedIcon fontSize="large" />
+              </Box>
+              <Typography>変更後</Typography>
+            </>
+          ) : (
+            <></>
+          )}
         </div>
+        {/* 新しい画像を未選択時には選択ボタン表示。選択後は非表示*/}
+        <div>
+          {!preview ? (
+            <>
+              <label htmlFor="icon-button-file">
+                <input
+                  accept="image/*"
+                  id="icon-button-file"
+                  type="file"
+                  hidden
+                  // 一度プレビューとアップロードをコメントアウトしてedit画面でvalueを編集できるようにする
+                  // onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  //   uploadImage(e);
+                  //   previewImage(e);
+                  // }}
+                  onChange={onChangeImage}
+                />
+
+                <IconButton color="inherit" component="span">
+                  <PhotoCameraIcon />
+                  <Typography>教材の写真を選択</Typography>
+                </IconButton>
+              </label>
+              <Box
+                sx={{
+                  display: "flex",
+
+                  "& > :not(style)": {
+                    m: 1,
+                    width: (260 * 4) / 3,
+                    height: 260,
+                    backgroundColor: "#EDF2F7",
+                  },
+                }}
+              >
+                <Paper elevation={3} />
+              </Box>
+            </>
+          ) : (
+            <></>
+          )}
+        </div>
+
+        {/* 新規作成時 */}
         <div>
           {preview ? (
             isLoading ? (
@@ -140,7 +197,12 @@ export const MaterialFormBody: FC<Props> = (props) => {
                     </IconButton>
                   </Box>
 
-                  <img src={preview} alt="preview img" height={260} />
+                  <img
+                    src={preview}
+                    alt="preview img"
+                    width={(260 * 4) / 3}
+                    height={260}
+                  />
                 </Box>
               </div>
             )
@@ -148,18 +210,21 @@ export const MaterialFormBody: FC<Props> = (props) => {
         </div>
 
         <div>
+          {/* 一度押したら画面遷移するまで押せない仕様に */}
           <Button
             type="submit"
             variant="outlined"
+            startIcon={startIcon}
             size="large"
+            fullWidth
             color="primary"
-            // disabled={!value.name || !value.description || !value.image}
             disabled={disabled}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                onClickSubmit();
-              }
-            }}
+            // onClick="disabled = true"
+            // onKeyDown={(e) => {
+            //   if (e.key === "Enter") {
+            //     onClickSubmit();
+            //   }
+            // }}
           >
             {children}
           </Button>
