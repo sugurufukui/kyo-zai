5abcb7b7daf6e12f7a191cb1f64938f9c789b0a1
sugurufukui <2ar.1225@gmail.com>
2023-03-01 07:54:10 +0900

    fix: paramsの値にdata=formDataの値を代入して情報をupdateするように設定

--- front/src/components/pages/material/Edit.tsx
+++ front/src/components/pages/material/Edit.tsx
@@ -16,12 +16,9 @@ export const Edit: FC = memo(() => {
   const [value, setValue] = useState({
     name: "",
     description: "",
-    preview: "",
     image: undefined,
   });
 
-  const [name, setName] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
   const [image, setImage] = useState<File>();
   const [preview, setPreview] = useState<
     string | ArrayBuffer | null | undefined
@@ -40,15 +37,13 @@ export const Edit: FC = memo(() => {
     try {
       const res = await getDetailMaterial(query.id);
       console.log("res.data", res.data);
-      // dataの中にurlの情報が入っている　http://localhost:3001/uploads/material/image/10/img_kyoto.jpeg
       console.log("res.data.image.url", res.data.image.url);
 
       // 使う値のみstateにセットする
       setValue({
         name: res.data.name,
         description: res.data.description,
-        preview: res.data.preview,
-        image: res.data.image,
+        image: res.data.image.url,
       });
     } catch (e) {
       console.log(e);
@@ -62,10 +57,9 @@ export const Edit: FC = memo(() => {
       [e.target.name]: e.target.value,
     });
     console.log("value", value);
-    console.log("e.target.name", e.target.name); //nameとdescription入力時には、e.target.nameとして、ここにそれぞれ、nameとdescriptionが表示される。image追加時には、e.target.nameにはなにも入っていないようで、なにも表示されない。
+    console.log("e.target.name", e.target.name);
   };
 
-  // 仮で設置した（formからの引用）
   // 画像選択機能
   const uploadImage = useCallback(
     (e) => {
@@ -74,72 +68,53 @@ export const Edit: FC = memo(() => {
       if (file.type.includes("image/")) {
         setImage(file);
         console.log("file", file);
-        console.log("image", image);
       } else {
         setImage(null);
         showSnackbar("そのファイルは登録できません", "error");
         return;
       }
     },
-    [setImage, showSnackbar, image]
+    [showSnackbar]
   );
 
   // プレビュー機能
-  const previewImage = useCallback(
-    (e) => {
-      const file = e.target.files[0];
-      setPreview(window.URL.createObjectURL(file));
-      console.log("file", file);
-      console.log("value.preview", value.preview);
-    },
-    [value.preview]
-  );
+  const previewImage = useCallback((e) => {
+    const file = e.target.files[0];
+    setPreview(window.URL.createObjectURL(file));
+    console.log("file", file);
+  }, []);
 
-  // 元のやつ
-  // // // 更新ボタン押下後、idとparameterをapiクライアントに渡し、リクエストを投げる
-  // const handleUpdatePost = async (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     const res = await updateMaterial(query.id, value);
-  //     console.log(res);
-  //     // リクエストが成功したら一覧画面に推移
-  //     history.push("/materials");
-  //     showSnackbar("編集しました", "success");
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // };
+  // 画像選択取り消し
+  const resetFile = useCallback(() => {
+    setImage(null);
+    setPreview(null);
+    console.log("image", image);
+    console.log("preview", preview);
+  }, [image, preview]);
 
   // FormData形式でデータを編集
   const updateFormData = (): FormData => {
     const formData = new FormData();
-
-    // formData.set("name", name);
-    // formData.set("description", description);
-    // if (image) formData.set("image", image);
-    // console.log("image", image);
-
-    // formData.append("name", name);
-    // formData.append("description", description);
+    // formDataにvalue内のnameとdescription、stateのimageを代入
+    formData.append("name", value.name);
+    formData.append("description", value.description);
     if (image) formData.append("image", image);
+    console.log("image", image);
     return formData;
   };
 
-  // 編集中
   // 更新ボタン押下後、idとparameterをapiクライアントに渡し、リクエストを投げる
-  // 教材更新時(初期で教材の情報が入っている状態にしたい)
   const handleUpdatePost = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
+      // 上で定義して3つの情報が入った入ったformDataをdataに代入
       const data = updateFormData();
-      console.log(data);
-      const res = await updateMaterial(query.id, value);
+      // paramsの値にdata=formDataの値を代入
+      const res = await updateMaterial(query.id, data);
       console.log(res);
-      // setPreview("");
-      // setImage(undefined);
       // リクエストが成功したら一覧画面に推移
       history.push("/materials");
-      showSnackbar("編集しました", "success");
+      showSnackbar("教材を編集しました", "success");
     } catch (e) {
       console.log(e);
     }
@@ -160,14 +135,12 @@ export const Edit: FC = memo(() => {
           handleChange(e);
         }}
         onChangeImage={(e: React.ChangeEvent<HTMLInputElement>) => {
-          uploadImage(e); //「教材の写真を選択」ボタンを押すと、ファイル選択画面が出る。setImageにFileをセットする
-          previewImage(e); //ファイル選択画面から画像を選択すると、プレビュー表示される
-          handleChange(e); //ファイルから画像を選択するたびに、valueの値を更新（更新できておらず、consoleの値は、なし）
+          uploadImage(e);
+          previewImage(e);
+          handleChange(e);
         }}
-        image={image}
-        setImage={setImage}
+        onClickResetFile={resetFile}
         preview={preview}
-        setPreview={setPreview}
         disabled={!value}
       />
       <Button variant="outlined" startIcon={<DeleteIcon />} color="error">
