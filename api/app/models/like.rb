cf6c7d865c9a8d9d8d7c2e31d1e145f1c0fbe8af
sugurufukui <2ar.1225@gmail.com>
2023-03-01 07:03:29 +0900

    fix: validates_uniqueness_of を validates :uniqueness: に変更

--- api/app/models/like.rb
+++ api/app/models/like.rb
@@ -3,5 +3,5 @@ class Like < ApplicationRecord
   belongs_to :material
 
   # 1人が１つの投稿に対して、１つしかいいねをつけられないようにする
-  validates_uniqueness_of :material_id, scope: :user_id
+  validates :material_id, uniqueness: { scope: :user_id }
 end
