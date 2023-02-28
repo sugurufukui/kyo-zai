5afad9c6711de2858daf5c2a2aae3919eed86630
sugurufukui <2ar.1225@gmail.com>
2023-03-01 07:12:31 +0900

    fix: 不要なコメントアウトを削除

--- front/src/lib/api/material.ts
+++ front/src/lib/api/material.ts
@@ -1,7 +1,5 @@
 import { client } from "lib/api/client";
 import Cookies from "js-cookie";
-// import { AxiosPromise } from "axios";
-// import { MaterialApiJson } from "types/api/materialType";
 
 // 教材一覧
 export const getAllMaterial = () => {
@@ -82,65 +80,3 @@ export const deleteMaterial = (id) => {
     },
   });
 };
-
-// import { client } from "lib/api/client";
-// import Cookies from "js-cookie";
-// import { AxiosPromise } from "axios";
-// // import { MaterialApiJson } from "types/api/materialType";
-
-// // 教材一覧
-// export const getAllMaterial = (): AxiosPromise<MaterialApiJson> => {
-//   return client.get("/materials");
-// };
-
-// // 自分がいいねした教材一覧
-// export const getMyLikeMaterial = () => {
-//   return client.get("/my_like", {
-//     headers: {
-//       "access-token": Cookies.get("_access_token"),
-//       client: Cookies.get("_client"),
-//       uid: Cookies.get("_uid"),
-//     },
-//   });
-// };
-
-// // 教材詳細
-// export const getDetailMaterial = (id) => {
-//   return client.get(`/materials/${id}`);
-// };
-
-// // 新規作成
-// export const createMaterial = (params: FormData): AxiosPromise => {
-//   return client.post("/materials", params, {
-//     // どのユーザーか判別するための情報
-//     headers: {
-//       "access-token": Cookies.get("_access_token"),
-//       client: Cookies.get("_client"),
-//       uid: Cookies.get("_uid"),
-//     },
-//   });
-// };
-
-// // 更新
-// export const updateMaterial = (id, params) => {
-//   return client.patch(`/materials/${id}`, params, {
-//     // どのユーザーか判別するための情報
-//     headers: {
-//       "access-token": Cookies.get("_access_token"),
-//       client: Cookies.get("_client"),
-//       uid: Cookies.get("_uid"),
-//     },
-//   });
-// };
-
-// // 削除
-// export const deleteMaterial = (id): AxiosPromise => {
-//   return client.delete(`/materials/${id}`, {
-//     // どのユーザーか判別するための情報
-//     headers: {
-//       "access-token": Cookies.get("_access_token"),
-//       client: Cookies.get("_client"),
-//       uid: Cookies.get("_uid"),
-//     },
-//   });
-// };
