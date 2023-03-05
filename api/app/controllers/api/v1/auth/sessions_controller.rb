70df437eeeb1ce1aac82c582c9b85f45c814b0f2
sugurufukui <2ar.1225@gmail.com>
2023-03-05 14:17:39 +0900

    fix: 不要なスペースを削除

--- api/app/controllers/api/v1/auth/sessions_controller.rb
+++ api/app/controllers/api/v1/auth/sessions_controller.rb
@@ -2,7 +2,6 @@
 class Api::V1::Auth::SessionsController < ApplicationController
   def index
     if current_api_v1_user
-
       render json: { is_login: true, data: current_api_v1_user }
     else
       render json: { is_login: false, message: "ユーザーが存在しません" }
--- api/config/initializers/carrierwave.rb
+++ api/config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 # 画像名に日本語が使えるようにする
-CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:].\-+]/
 
 CarrierWave.configure do |config|
   if Rails.env.production? # 本番環境の場合はS3へアップロード
@@ -19,7 +19,7 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: ENV.fetch('S3_ACCESS_KEY_ID', nil), # アクセスキー
       aws_secret_access_key: ENV.fetch('S3_SECRET_ACCESS_KEY', nil), # シークレットアクセスキー
-      region: 'ap-northeast-1',  # アジアパシフィック(東京)
+      region: 'ap-northeast-1', # アジアパシフィック(東京)
       path_style: true
     }
   else # 本番環境以外の場合はアプリケーション内にアップロード
