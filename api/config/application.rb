2f9557e3ad2c44c7d5faf4e8094849a2e054b6af
sugurufukui <2ar.1225@gmail.com>
2023-03-31 15:27:03 +0900

    fix: バラバラに設定していたcorsの設定をcors.rbの1箇所に集約

--- api/config/application.rb
+++ api/config/application.rb
@@ -24,17 +24,6 @@ module KyoZai
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # rack-cors middleware
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins ENV.fetch('ALLOWED_ORIGINS', 'http://localhost:3000')
-        resource '*',
-                 headers: :any,
-                 methods: %i[get post put patch delete options head],
-                 credentials: true
-      end
-    end
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
