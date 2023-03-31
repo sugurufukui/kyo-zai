2d0ca352e8e7d462305625d776550493da0e6dee
sugurufukui <2ar.1225@gmail.com>
2023-03-31 15:27:03 +0900

    fix: バラバラに設定していたcorsの設定をcors.rbの1箇所に集約

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -66,14 +66,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  # CORSの設定を追加する
-  config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'localhost:3000'
-      resource '*',
-               headers: :any,
-               methods: %i[get post put patch delete options head]
-    end
-  end
 end
--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', "https://web.tokushi-kyouzai.com"
-    # Reactのポート/ web.取得したドメイン名(形式でフロントエンドのサブドメインを設定)
+    origins 'localhost:3000', 'https://tokushi-kyouzai.com'
+    # Reactのポート/ 取得したドメイン名(形式でフロントエンドのサブドメインを設定)
 
     resource '*',
       headers: :any,
