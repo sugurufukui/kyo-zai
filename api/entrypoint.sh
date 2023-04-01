6b6af249014c412f8f16f2e8e4b396776fcc9edf
sugurufukui <2ar.1225@gmail.com>
2023-04-01 17:21:12 +0900

    add: 本番環境の設定を追加

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -5,14 +5,14 @@ set -e
 rm -f /kyo-zai/tmp/pids/server.pid
 
 # デプロイ初回のみ実行
-bundle exec rails db:create
-bundle exec rails db:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+# bundle exec rails db:create
+# bundle exec rails db:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 
 bundle exec rails db:migrate
 
 # デプロイ初回のみ実行(デプロイ先の本番環境でDBに初期データを投入する)
-bundle exec rails db:seed RAILS_ENV=production
+# bundle exec rails db:seed RAILS_ENV=production
 
 
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
+# DockerfileのCMDで指定されたコマンド（この場合、bundle exec unicorn -p 3000 -c /kyo-zai/api/config/unicorn.rb -E production）が実行され、RailsアプリケーションがUnicornサーバー上で起動される
 exec "$@"
