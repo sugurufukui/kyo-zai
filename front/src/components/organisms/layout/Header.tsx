916abc49d55430ebf5e502eb3f5b1462dad7eaa5
sugurufukui <2ar.1225@gmail.com>
2023-04-01 18:18:40 +0900

    update: Headerの一部をコンポーネント化してリファクタリング

--- front/src/components/organisms/layout/Header.tsx
+++ front/src/components/organisms/layout/Header.tsx
@@ -1,3 +1,4 @@
+// front/src/components/organisms/layout/Header.tsx
 import React, { FC, memo, useContext, useEffect, useState } from "react";
 import { useHistory, Link } from "react-router-dom";
 import Cookies from "js-cookie";
@@ -5,40 +6,27 @@ import Cookies from "js-cookie";
 import AppBar from "@mui/material/AppBar";
 import Typography from "@mui/material/Typography";
 import Toolbar from "@mui/material/Toolbar";
-import Button from "@mui/material/Button";
-import {
-  Avatar,
-  Box,
-  Container,
-  Divider,
-  IconButton,
-  ListItemIcon,
-  MenuItem,
-  SwipeableDrawer,
-} from "@mui/material";
+import { Box, Container, SwipeableDrawer } from "@mui/material";
 import { HamburgerButton } from "components/atoms/button/HamburgerButton";
+import { useSnackbar } from "providers/SnackbarProvider";
+import { signOut } from "lib/api/auth";
+import { AuthContext } from "providers/AuthProvider";
+import LogoIcon from "images/top.png";
+import { AuthButtons } from "components/molecules/AuthButtons";
+import { AvatarMenu } from "components/atoms/AvatarMenu";
 import HomeIcon from "@mui/icons-material/Home";
-import LoginIcon from "@mui/icons-material/Login";
-import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import MenuBookIcon from "@mui/icons-material/MenuBook";
 import PostAddIcon from "@mui/icons-material/PostAdd";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import LoginIcon from "@mui/icons-material/Login";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import LogoutIcon from "@mui/icons-material/Logout";
-import Menu from "@mui/material/Menu";
-
-import { signOut } from "lib/api/auth";
-
-import { AuthContext } from "providers/AuthProvider";
-
-import LogoIcon from "images/top.png";
-import { useSnackbar } from "providers/SnackbarProvider";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
 export const Header: FC = memo(() => {
-  //アバターメニュー関係
+  // アバターメニュー関係
   const [avatarMenuOpened, setAvatarMenuOpened] = useState(undefined);
-  const onClickAvatar = (e: React.MouseEvent<HTMLElement>) => {
-    setAvatarMenuOpened(e.currentTarget);
+  const onClickAvatar = () => {
+    setAvatarMenuOpened(true);
   };
   const onCloseAvatarMenu = () => {
     setAvatarMenuOpened(undefined);
@@ -50,16 +38,11 @@ export const Header: FC = memo(() => {
 
   const { loading, isSignedIn, setIsSignedIn, currentUser } =
     useContext(AuthContext);
-
   const history = useHistory();
 
-  // if文 三項演算子でかけるか？
-  // auth ? authPages : noAuthPages;
-  // 共通しているものは一つにまとめるか？
   const authPages = [
     { children: "HOME", icon: <HomeIcon />, link: "/" },
     { children: "教材一覧", icon: <MenuBookIcon />, link: "/materials" },
-    // { children: "検索", icon: <SearchIcon />, link: "/" },
     { children: "投稿", icon: <PostAddIcon />, link: "/materials/new" },
   ];
   const noAuthPages = [
@@ -67,6 +50,7 @@ export const Header: FC = memo(() => {
     { children: "新規登録", icon: <PersonAddIcon />, link: "/signup" },
     { children: "ログイン", icon: <LoginIcon />, link: "/signin" },
   ];
+
   const avatarMenu = [
     {
       children: "マイページ",
@@ -81,13 +65,11 @@ export const Header: FC = memo(() => {
     },
   ];
 
-  // サインアウトボタンをクリックした時
-  const onClickSignOut = async (event) => {
+  const onClickSignOut = async () => {
     try {
       const res = await signOut();
 
       if (res.data.success === true) {
-        // サインアウト時には各Cookieを削除
         Cookies.remove("_access_token");
         Cookies.remove("_client");
         Cookies.remove("_uid");
@@ -95,64 +77,12 @@ export const Header: FC = memo(() => {
         setIsSignedIn(false);
         history.push("/signin");
 
-        // ログアウトの確認モーダルを表示
         showSnackbar("ログアウトしました", "success");
-        console.log("ログアウトしました");
-        console.log(res);
       } else {
         showSnackbar("ログアウトできませんでした", "error");
       }
     } catch (err) {
       showSnackbar("ログアウトできませんでした", "error");
-    }
-  };
-
-  // onClickSignOutと一緒にAuthButtonとしてコンポーネント化する：どのディレクトリに分けるべきか
-  const AuthButtons = () => {
-    // 認証完了後はサインアウト用のボタンを表示
-    // 未認証時は認証用のボタンを表示
-    if (!loading) {
-      if (isSignedIn) {
-        return (
-          //サインインしている状態のボタン=>①HOME②教材一覧③検索④投稿する⑤マイページ（アイコン（アイコンクリック時にモーダル出現））
-          <>
-            {authPages.map((authPage) => (
-              <Button
-                key={authPage.children}
-                startIcon={authPage.icon}
-                component={Link}
-                to={authPage.link}
-                sx={{ my: 2, mx: 1, color: "white", display: "flex" }}
-                size="large"
-                onClick={() => setDrawerOpened(false)}
-              >
-                {authPage.children}
-              </Button>
-            ))}
-          </>
-        );
-      } else {
-        // 未ログイン時
-        return (
-          <>
-            {noAuthPages.map((noAuthPage) => (
-              <Button
-                key={noAuthPage.children}
-                startIcon={noAuthPage.icon}
-                component={Link}
-                to={noAuthPage.link}
-                sx={{ my: 2, mx: 1, color: "white", display: "flex" }}
-                onClick={() => setDrawerOpened(false)}
-              >
-                {noAuthPage.children}
-              </Button>
-            ))}
-          </>
-        );
-      }
-    } else {
-      // ローディング終了時
-      return <></>;
     }
   };
 
@@ -177,55 +107,23 @@ export const Header: FC = memo(() => {
                 </Link>
               </Typography>
               <Box sx={{ display: { xs: "none", md: "flex" } }}>
-                <AuthButtons />
+                <AuthButtons
+                  loading={loading}
+                  isSignedIn={isSignedIn}
+                  authPages={authPages}
+                  noAuthPages={noAuthPages}
+                  setDrawerOpened={setDrawerOpened}
+                />
               </Box>
-              {/* アバターボタン */}
-              {/* ログイン時のみ出現させるために三項演算子で分岐。そうすることでログイン時にavatarメニューが左上に開いてしまう */}
-              {isSignedIn ? (
-                <Box sx={{ flexGrow: 0 }}>
-                  <IconButton
-                    key={currentUser?.id}
-                    size="large"
-                    onClick={onClickAvatar}
-                    sx={{ p: 1.5 }}
-                  >
-                    <Avatar />
-                  </IconButton>
-                  <Menu
-                    sx={{ mt: "45px" }}
-                    id="menu-appbar"
-                    anchorEl={avatarMenuOpened}
-                    anchorOrigin={{ vertical: "top", horizontal: "right" }}
-                    keepMounted
-                    transformOrigin={{
-                      vertical: "top",
-                      horizontal: "right",
-                    }}
-                    open={Boolean(avatarMenuOpened)}
-                    onClose={onCloseAvatarMenu}
-                  >
-                    {avatarMenu.map((userMaterial) => (
-                      <MenuItem
-                        key={userMaterial.children}
-                        component={Link}
-                        to={userMaterial.link}
-                        onClick={onCloseAvatarMenu}
-                      >
-                        <ListItemIcon>{userMaterial.icon}</ListItemIcon>
-                        <Typography textAlign="center">
-                          {userMaterial.children}
-                        </Typography>
-                      </MenuItem>
-                    ))}
-                    <Divider />
-                    <MenuItem key="ログアウト" onClick={onClickSignOut}>
-                      <LogoutIcon color="action" />
-                      <Typography align="center">ログアウト</Typography>
-                    </MenuItem>
-                  </Menu>
-                </Box>
-              ) : (
-                <></>
+              {isSignedIn && (
+                <AvatarMenu
+                  currentUser={currentUser}
+                  avatarMenuOpened={avatarMenuOpened}
+                  onClickAvatar={onClickAvatar}
+                  onCloseAvatarMenu={onCloseAvatarMenu}
+                  avatarMenu={avatarMenu}
+                  onClickSignOut={onClickSignOut}
+                />
               )}
 
               <HamburgerButton onOpen={() => setDrawerOpened(true)} />
@@ -243,7 +141,13 @@ export const Header: FC = memo(() => {
                 }}
                 sx={{ width: "30%" }}
               >
-                <AuthButtons />
+                <AuthButtons
+                  loading={loading}
+                  isSignedIn={isSignedIn}
+                  authPages={authPages}
+                  noAuthPages={noAuthPages}
+                  setDrawerOpened={setDrawerOpened}
+                />
               </SwipeableDrawer>
             </Toolbar>
           </Container>
