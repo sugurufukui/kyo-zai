5349916f43620bcc20e7596c1f9918b784a8f377
sugurufukui <2ar.1225@gmail.com>
2023-04-03 11:12:13 +0900

    add: 画像選択時にサイズや画質を調整

--- front/src/components/pages/material/New.tsx
+++ front/src/components/pages/material/New.tsx
@@ -6,6 +6,7 @@ import { useSnackbar } from "providers/SnackbarProvider";
 import PostAddIcon from "@mui/icons-material/PostAdd";
 import { Button } from "@mui/material";
 import ReplyIcon from "@mui/icons-material/Reply";
+import { resizeImage } from "lib/imageResizeUtil";
 
 export const New: FC = memo(() => {
   const history = useHistory();
@@ -21,25 +22,21 @@ export const New: FC = memo(() => {
   const { showSnackbar } = useSnackbar();
 
   // 画像選択機能
-  const uploadImage = useCallback((e) => {
+  const uploadImage = useCallback(async (e) => {
     const file = e.target.files[0];
-    // image以外のファイルはnullにしてプレビューさせずにアラート表示;
-    if (file.type.includes("image/")) {
-      setImage(file);
-      console.log(file);
-    } else {
+    if (!file.type.includes("image/")) {
       setImage(null);
       showSnackbar("そのファイルは登録できません", "error");
       return;
     }
-  }, []);
 
-  // プレビュー機能
-  const previewImage = useCallback((e) => {
-    const file = e.target.files[0];
-    setPreview(window.URL.createObjectURL(file));
-    console.log(file);
-    console.log(preview);
+    try {
+      const resizedImage = await resizeImage(file, 1024, 1024);
+      setImage(resizedImage);
+      setPreview(window.URL.createObjectURL(resizedImage));
+    } catch (error) {
+      showSnackbar("画像のリサイズに失敗しました。", "error");
+    }
   }, []);
 
   // 画像選択取り消し
@@ -74,18 +71,12 @@ export const New: FC = memo(() => {
       console.log(e);
     }
   };
+
   return (
     <>
       <MaterialFormBody
         title="教材新規作成"
-        onClickSubmit={(e) => {
-          e.preventDefault(); // フォームの送信をブロック
-          if (name.length <= 30 && description && image) {
-            handleCreatePost(e);
-          } else {
-            showSnackbar("名前は30文字以内で作成してください", "error");
-          }
-        }}
+        onClickSubmit={handleCreatePost}
         value={value}
         children="教材を登録する"
         startIcon={<PostAddIcon />}
@@ -95,8 +86,7 @@ export const New: FC = memo(() => {
             setName(name);
             setValue((prev) => ({ ...prev, name }));
           } else {
-            // 入力した内容を保存して31文字以上は入力できないようにする
-            const prevName = name.slice(0, 30); // 入力文字列の先頭から30文字を抜き出す
+            const prevName = name.slice(0, 30);
             setName(prevName);
             setValue((prev) => ({ ...prev, name: prevName }));
           }
@@ -106,10 +96,7 @@ export const New: FC = memo(() => {
           setDescription(description);
           setValue((prev) => ({ ...prev, description }));
         }}
-        onChangeImage={(e: React.ChangeEvent<HTMLInputElement>) => {
-          uploadImage(e);
-          previewImage(e);
-        }}
+        onChangeImage={uploadImage}
         onClickResetFile={resetFile}
         image={image}
         preview={preview}
