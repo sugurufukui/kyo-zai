eb7c51d19adf3100b0ee6b65f693f89d78823e32
sugurufukui <2ar.1225@gmail.com>
2023-04-06 22:12:25 +0900

    update: 不要なコメントを削除

--- front/src/lib/api/client.ts
+++ front/src/lib/api/client.ts
@@ -1,11 +1,6 @@
 import axios from "axios";
 import applyCaseMiddleware from "axios-case-converter";
 
-// applyCaseMiddleware:
-// axiosで受け取ったレスポンスの値をスネークケース→キャメルケースに変換
-// または送信するリクエストの値をキャメルケース→スネークケースに変換してくれるライブラリ
-
-// ヘッダーに関してはケバブケースのままで良いので適用を無視するオプションを追加
 const options = {
   ignoreHeaders: true,
 };
