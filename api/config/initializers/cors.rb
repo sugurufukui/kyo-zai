2dd91ace710a74ff02d64fe4c34a275070e76129
sugurufukui <2ar.1225@gmail.com>
2023-04-06 18:51:05 +0900

    add: サブドメインの追加

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -1,7 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'https://tokushi-kyouzai.com'
-    # Reactのポート/ 取得したドメイン名(形式でフロントエンドのサブドメインを設定)
+    origins 'localhost:3000', 'https://tokushi-kyouzai.com', 'https://*.tokushi-kyouzai.com'
 
     resource '*',
       headers: :any,
