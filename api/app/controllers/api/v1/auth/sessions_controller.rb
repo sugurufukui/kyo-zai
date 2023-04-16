3a610f02bc1ea49b758a6c90d410120771ee43cc
sugurufukui <2ar.1225@gmail.com>
2023-04-16 11:41:05 +0900

    fix: 不要なコメントや記述を削除、コード入れ替え

--- api/app/controllers/api/v1/auth/sessions_controller.rb
+++ api/app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,6 +1,6 @@
 # ログイン状態確認用コントローラー
 class Api::V1::Auth::SessionsController < DeviseTokenAuth::SessionsController
-  #ユーザーがサインインしてるか確認
+  # ユーザーがサインインしてるか確認
   def index
     if current_api_v1_user
       render json: { is_login: true, data: current_api_v1_user }
