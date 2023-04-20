499b941ec549afbf4e827d374a0bf870a28f35af
sugurufukui <2ar.1225@gmail.com>
2023-04-21 07:57:02 +0900

    fix: seedDATA削除

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -8,13 +8,13 @@ if [ "${RAILS_ENV}" = "production" ]
 then
   # デプロイ初回のみ実行
   # bundle exec rails db:create
+  bundle exec rails db:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
   bundle exec rails db:migrate
   # bundle exec rails db:seed RAILS_ENV=production
 else
   # 開発環境では、データベースのリセットも実行する
   bundle exec rails db:create
   bundle exec rails db:migrate
-  bundle exec rails db:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 fi
 
 # DockerfileのCMDで指定されたコマンド（この場合、bundle exec unicorn -p 3000 -c /kyo-zai/config/unicorn.rb -E production）が実行され、RailsアプリケーションがUnicornサーバー上で起動される
