ce96e04162c9a08aa88cb21f219d2d740e06b7a9
sugurufukui <2ar.1225@gmail.com>
2023-04-25 16:20:17 +0900

    update: ログインユーザーは一覧ページへ遷移

--- front/src/router/Router.tsx
+++ front/src/router/Router.tsx
@@ -1,5 +1,5 @@
-import { FC, memo, ReactElement } from "react";
-import { Redirect, Route, Switch } from "react-router-dom";
+import { FC, memo, ReactElement, useEffect } from "react";
+import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 
 import { Account } from "components/pages/user/Account";
 import { AccountEdit } from "components/pages/user/AccountEdit";
@@ -24,7 +24,18 @@ import { AuthProvider } from "providers/AuthProvider";
 
 export const Router: FC = memo(() => {
   const { isSignedIn } = useAuth();
+  const history = useHistory();
 
+  useEffect(() => {
+    console.log("Router isSignedIn:", isSignedIn);
+    if (isSignedIn) {
+      history.push("/materials");
+    }
+  }, [isSignedIn, history]);
+
+  if (isSignedIn === null) {
+    return null;
+  }
   // 未認証の場合「/signin」ページへ
   type Props = {
     children: ReactElement;
@@ -40,42 +51,40 @@ export const Router: FC = memo(() => {
   };
 
   return (
-    <Switch>
-      <AuthProvider>
-        <CommonLayout>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/signup" component={SignUp} />
-            <Route exact path="/welcome" component={WelcomeEmail} />
-            <Route exact path="/signin" component={SignIn} />
-            <Route exact path="/forgot_password" component={ForgotPassword} />
-            <Route exact path="/send_password_email" component={ResetPasswordEmail} />
-            <Route exact path="/reset_password/:token" component={ResetPassword} />
-            <Route exact path="/expired_email_link" component={ExpiredEmailLink} />
-            <Private>
-              <Switch>
-                <Route exact path="/materials" component={List} />
-                <Route exact path="/liked_materials" component={Liked} />
-                <Route exact path="/materials/new" component={New} />
-                <Route exact path="/materials/:id" component={Detail} />
-                <Route exact path="/mine_materials" component={Mine} />
-                <Route exact path="/materials/edit/:id" component={Edit} />
-                <Route exact path="/user/:id" component={Account} />
-                <Route exact path="/user/edit/:id" component={AccountEdit} />
-                <Route>
-                  <Page404 />
-                </Route>
-                <Route exact path="/notfound404">
-                  <Page404 />
-                </Route>
-              </Switch>
-            </Private>
-            <Route>
-              <Page404 />
-            </Route>
-          </Switch>
-        </CommonLayout>
-      </AuthProvider>
-    </Switch>
+    <AuthProvider>
+      <CommonLayout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/welcome" component={WelcomeEmail} />
+          <Route exact path="/signin" component={SignIn} />
+          <Route exact path="/forgot_password" component={ForgotPassword} />
+          <Route exact path="/send_password_email" component={ResetPasswordEmail} />
+          <Route exact path="/reset_password/:token" component={ResetPassword} />
+          <Route exact path="/expired_email_link" component={ExpiredEmailLink} />
+          <Private>
+            <Switch>
+              <Route exact path="/materials" component={List} />
+              <Route exact path="/liked_materials" component={Liked} />
+              <Route exact path="/materials/new" component={New} />
+              <Route exact path="/materials/:id" component={Detail} />
+              <Route exact path="/mine_materials" component={Mine} />
+              <Route exact path="/materials/edit/:id" component={Edit} />
+              <Route exact path="/user/:id" component={Account} />
+              <Route exact path="/user/edit/:id" component={AccountEdit} />
+              <Route>
+                <Page404 />
+              </Route>
+              <Route exact path="/notfound404">
+                <Page404 />
+              </Route>
+            </Switch>
+          </Private>
+          <Route>
+            <Page404 />
+          </Route>
+        </Switch>
+      </CommonLayout>
+    </AuthProvider>
   );
 });
